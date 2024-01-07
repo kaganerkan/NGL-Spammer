ce2079a0f04febac0bda9239670256339298d086
kaganerkan <98615819+kaganerkan@users.noreply.github.com>
2024-01-07 15:18:29 +0300

    Update main.py
    -Now when Cloud Flare blocks rather than crash it basically stops and waits a little and tries later.
    -Now it's a headless browser so doesn't interfier with you while you do your job.

--- main.py
+++ main.py
@@ -6,25 +6,34 @@ import random
 import string
 import time
 
-Target = str(input("Account to target:\n"))
+#Target = str(input("Account to target:\n"))
 
-options = Options()
+def system():
+    options = Options()
+    options.headless = True
+    driver = uc.Chrome(options=options)
 
-driver = uc.Chrome(options=options)
+    def detect_label_and_submit(driver):
+        text_box = driver.find_element(By.ID, "question")
+        random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        text_box.send_keys(random_string)
+        submit_button = driver.find_element(By.CLASS_NAME, "submit")
+        submit_button.click()
 
-def detect_label_and_submit(driver):
-    text_box = driver.find_element(By.ID, "question")
-    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-    text_box.send_keys(random_string)
-    submit_button = driver.find_element(By.CLASS_NAME, "submit")
-    submit_button.click()
+    driver.get("https://ngl.link/"+Target)
+    driver.maximize_window()
 
-driver.get("https://ngl.link/"+Target)
-driver.maximize_window()
+    while True:
+        try:
+            detect_label_and_submit(driver)
+            time.sleep(random.uniform(2, 2.5))
+            send_another_link = driver.find_element(By.CLASS_NAME, "another1")
+            send_another_link.click()
+            time.sleep(random.uniform(2, 2.5))
+        except:
+            driver.close()
+            time.sleep(random.uniform(25,60))
+            system()
 
-while True:
-    detect_label_and_submit(driver)
-    time.sleep(random.uniform(2, 2.5))
-    send_another_link = driver.find_element(By.CLASS_NAME, "another1")
-    send_another_link.click()
-    time.sleep(random.uniform(2, 2.5))
+Target = input("Target:\n")
+system()
